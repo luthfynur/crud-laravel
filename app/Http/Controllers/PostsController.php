8b8a5fee3c39bf157b575dcab289b3668d0ad2df
luthfynur <nurluthfy98@gmail.com>
2021-06-06 20:51:03 +0700

    Add index post view

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,7 +14,9 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Posts::all();
+        
+        return view('post-index',compact('posts'));
     }
 
     /**
---
+++ resources/views/post-index.blade.php
@@ -0,0 +1,45 @@
+@extends('post-layout')
+
+@section('post-content')
+<style>
+  .uper {
+    margin-top: 40px;
+  }
+</style>
+<div class="uper">
+  @if(session()->get('success'))
+    <div class="alert alert-success">
+      {{ session()->get('success') }}  
+    </div><br />
+  @endif
+  <table class="table table-striped">
+    <thead>
+        <tr>
+          <td>ID</td>
+          <td>Title</td>
+          <td>Subtitle</td>
+          <td>Content</td>
+          <td colspan="2">Action</td>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($posts as $post)
+        <tr>
+            <td>{{$post->id}}</td>
+            <td>{{$post->title}}</td>
+            <td>{{$post->sub_title}}</td>
+            <td>{{$post->body}}</td>
+            <td><a href="{{ route('posts.edit', $post->id)}}" class="btn btn-primary">Edit</a></td>
+            <td>
+                <form action="{{ route('posts.destroy', $post->id)}}" method="post">
+                  @csrf
+                  @method('DELETE')
+                  <button class="btn btn-danger" type="submit">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+  </table>
+<div>
+@endsection
