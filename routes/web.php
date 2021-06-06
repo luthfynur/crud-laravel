67d300ef4f3663007340e79b242e4079f3f863ff
luthfynur <nurluthfy98@gmail.com>
2021-06-06 21:42:27 +0700

    Add Category route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,5 @@ Route::get('/', function () {
 });
 
 Route::resource('posts', PostsController::class);
+
+Route::resource('categories', CategoryController:class);
