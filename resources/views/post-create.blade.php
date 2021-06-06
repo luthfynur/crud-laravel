fc977ea4b41458a970b4d2417de6167518aca7b0
luthfynur <nurluthfy98@gmail.com>
2021-06-06 20:04:14 +0700

    Add post create view

---
+++ resources/views/post-create.blade.php
@@ -0,0 +1,41 @@
+@extends('post-layout')
+
+@section('post-content')
+<style>
+  .uper {
+    margin-top: 40px;
+  }
+</style>
+<div class="card uper">
+  <div class="card-header">
+    Add New Post
+  </div>
+  <div class="card-body">
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+      </div><br />
+    @endif
+      <form method="post" action="{{ route('posts.store') }}">
+          <div class="form-group">
+              @csrf
+              <label for="post-title">Post Title:</label>
+              <input type="text" class="form-control" name="title"/>
+          </div>
+          <div class="form-group">
+              <label for="post-subtitle">Sub Title :</label>
+              <input type="text" class="form-control" name="subtitle"/>
+          </div>
+          <div class="form-group">
+              <label for="post-body">Content :</label>
+              <input type="text" class="form-control" name="body"/>
+          </div>
+          <button type="submit" class="btn btn-primary">Create</button>
+      </form>
+  </div>
+</div>
+@endsection
