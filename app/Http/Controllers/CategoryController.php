da6aee5da08140a5e90d21ec2e9593c0b75eb984
luthfynur <nurluthfy98@gmail.com>
2021-06-06 22:05:11 +0700

    Update category edit controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -64,7 +64,9 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $category = Category::findOrFail($id);
+
+        return view('category-edit', compact('category'));
     }
 
     /**
