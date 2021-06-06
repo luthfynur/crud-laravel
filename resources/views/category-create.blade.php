b3f28610be804723ec47600c29a7e79271b0afa3
luthfynur <nurluthfy98@gmail.com>
2021-06-06 21:48:55 +0700

    Add create category view

---
+++ resources/views/category-create.blade.php
@@ -0,0 +1,33 @@
+@extends('category-layout')
+
+@section('category-content')
+<style>
+  .uper {
+    margin-top: 40px;
+  }
+</style>
+<div class="card uper">
+  <div class="card-header">
+    Add New Category
+  </div>
+  <div class="card-body">
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+      </div><br />
+    @endif
+      <form method="post" action="{{ route('categories.store') }}">
+          <div class="form-group">
+              @csrf
+              <label for="category-name">Category name:</label>
+              <input type="text" class="form-control" name="name"/>
+          </div>
+          <button type="submit" class="btn btn-primary">Create</button>
+      </form>
+  </div>
+</div>
+@endsection
